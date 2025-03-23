10b7c5b43387b09beb8077a8a6a99238c5d6cb9d
sandrorsjunior <hulk@DESKTOP-PLGLQAA>
2025-03-23 20:18:37 +0000

    create postcontroller

--- src/main/java/com/sandrorjr/myBlog/controller/PostController.java
+++ src/main/java/com/sandrorjr/myBlog/controller/PostController.java
@@ -1,4 +1,26 @@
 package com.sandrorjr.myBlog.controller;
 
+import com.sandrorjr.myBlog.model.PostModel;
+import com.sandrorjr.myBlog.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("postArticle")
 public class PostController {
+    @Autowired
+    private PostRepository postRepository;
+
+    @GetMapping
+    public List<PostModel> getAllPost(){
+        return postRepository.findAll();
+    }
+
+    @PostMapping
+    public PostModel savePost(@RequestBody PostModel post){
+        postRepository.save(post);
+        return post;
+    }
 }
